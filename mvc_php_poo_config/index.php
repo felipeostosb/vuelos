30be5883af22467a1df2a515124eb91583eeea6e
jesusespejoc-design <jesus.espejoc@istpargentina.edu.pe>
2026-05-31 23:19:53 +0000

    Hola mundo

--- mvc_php_poo_config/index.php
+++ mvc_php_poo_config/index.php
@@ -1,22 +1,3 @@
 <?php
-
-declare(strict_types=1);
-
-require_once __DIR__ . '/controllers/UserController.php';
-
-// Cambiamos el valor por defecto a 'home'
-$action = $_GET['action'] ?? 'home';
-
-$controller = new UserController();
-
-// El 'match' decide qué función ejecutar según la URL
-match ($action) {
-    'insertar'   => $controller->insertar(),
-    'formulario' => $controller->mostrarFormulario(),
-    'home'       => $controller->showHome(),
-    'destinos'   => $controller->showDestinos(),
-    'ofertas'    => $controller->showOfertas(),
-    'checkin'    => $controller->showCheckin(),
-    'ayuda'      => $controller->showAyuda(),
-    default      => $controller->showHome(),
-};
+echo"Hola mundo";
+?>
